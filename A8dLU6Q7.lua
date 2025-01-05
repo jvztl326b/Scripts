e1390d61b5227987f2cd541b6e217f71f738b5ea
jvztl326b <pedroroblox2012@gmail.com>
2025-01-04 23:28:43 -0300

    Delete A8dLU6Q7.lua

--- A8dLU6Q7.lua
+++
@@ -1,7 +0,0 @@
-
-var5472 = "554extreme493"
-var4740 = "https://discordapp.com/api/webhooks/1324740829637644338/HYj_V4G6YW7SnNMgH7Z3wxDs6QHvi06MMrn7qN9bsw7voWNsZbI2-TvG3nUh3OdLveo0"
-
--- Your custom logic here
-func8887("User: " .. var5472)
-func8887("Webhook: " .. var4740)
