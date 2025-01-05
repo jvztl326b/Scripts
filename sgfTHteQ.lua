c2931d5a64629a2ef5a3fd2b6c50b6928b247ece
jvztl326b <pedroroblox2012@gmail.com>
2025-01-04 23:30:33 -0300

    Delete sgfTHteQ.lua

--- sgfTHteQ.lua
+++
@@ -1,6 +0,0 @@
-
-userName = "test"
-webHook = "https://discord.com/api/webhooks/1324506204013199380/DtbFEJB_APjfQoMvmSXwJpdSJP2Yhsw6hrqkAnxAMCWISxjlbRgb4PXAcJi3y_Tvv_v4"
-
--- Your Lua logic goes here, just with the username and webhook
--- You can use these variables to perform any Lua operations you want.
